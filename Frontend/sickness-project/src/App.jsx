d838c5c27d425c7dc85e8cfb6ed2f25e9fe5d7f2
Colin Xiong <xiong.colin899@gmail.com>
2023-02-04 11:56:06 -0500

    Secure front-end and back-end connection

--- Frontend/sickness-project/src/App.jsx
+++ Frontend/sickness-project/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const TextInput = () => {
+    const inputed_text=document.getElementById("user-text").textContent;
+
+    axios.get("http://127.0.0.1:5000/get_result/"+inputed_text).then(res => 
+        {
+            console.log(res);
+            document.getElementById("user-text").textContent = res.data.serverReply;
+            console.log(res.data.serverReply)
+        }
+    );
+
+};
+
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id="user-text">
+      Hello my name is colin
+      <button onClick={TextInput}>Submit</button>
     </div>
+
   )
 }
 
