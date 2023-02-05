61f00960c71e6a6580cec87b360a2d6ba9dcc2a5
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2023-02-05 09:25:37 -0500

    addded cs

--- Frontend/sickness-project/src/components/Response.jsx
+++ Frontend/sickness-project/src/components/Response.jsx
@@ -3,12 +3,12 @@ import precautions from "../../../../data/precautions.json"
 import { useState, useEffect } from "react"
 import { Box, Typography, List, ListItem, ListItemText } from "@mui/material"
 import Virus from "../assets/virus.svg"
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 AOS.init();
 
-const Response = ({word}) => {
-    console.log(word);
+const Response = ({word, score}) => {
     const [text, setText] = useState('')
     const [Multiprecautions, setMultiPrecautions] = useState([
         {precaution1: '', precaution2: '', precaution3: '', precaution4: ''},
@@ -26,9 +26,25 @@ const Response = ({word}) => {
           {precaution1: precautionsDef1, precaution2: precautionsDef2, precaution3: precautionsDef3, precaution4: precautionsDef4});
     }, [word])
 
+
+    const ScoreDisplay = ({ score }) => {
+      let displayText;
+      if (score <= 10) {
+        displayText = "... You dont't have to worry take a rest at home";
+      } else if (score <= 20) {
+        displayText = "... Maybe you should get an appointment with a doctor";
+      } else if (score <= 35) {
+        displayText = "... You need urgent medical attention";
+      } 
+    
+      return <Typography variant="h6">{displayText}</Typography>;
+    };
+    
+
   return (
     <Box m="30px" backgroundColor="#f7f7f7" textAlign="left" borderRadius="15px" boxShadow={1} data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">
         <Typography m="60px" fontFamily="Poppins" fontWeight="600" fontSize="2rem">Our prediction: <Typography marginLeft="5px"fontFamily="Poppins" fontWeight="600" fontSize="2rem" color="grey">{word.toUpperCase()}</Typography></Typography>
+        <Typography m="60px" fontFamily="Poppins" fontWeight="600" fontSize="2rem">Your M.I.D Score:<Typography marginLeft="5px"fontFamily="Poppins" fontWeight="600" fontSize="2rem" color="grey">{score}<ScoreDisplay score={score}/> </Typography></Typography>
         <Typography marginRight="60px" marginLeft="60px" fontFamily="Poppins" fontWeight="400" textAlign="left">
             {text}
         </Typography>
@@ -42,7 +58,7 @@ const Response = ({word}) => {
                 <ListItemText primary={<Typography fontFamily="roboto" fontWeight="700" p="5px" m="5px" backgroundColor="white" width="100px" borderRadius="20px" textAlign="center" boxShadow={2}>Precaution 2</Typography>} secondary={<Typography fontFamily="roboto" fontWeight="300" paddingLeft="15px"> ○ {Multiprecautions.precaution2}</Typography>}/>
             </ListItem>
             <ListItem>
-            <ListItemText primary={<Typography fontFamily="roboto" fontWeight="700" p="5px" m="5px" backgroundColor="white" width="100px" borderRadius="20px" textAlign="center" boxShadow={2}>Precaution 3</Typography>} secondary={<Typography fontFamily="roboto" fontWeight="300" paddingLeft="15px"> ○ {Multiprecautions.precaution3}</Typography>}/>
+                <ListItemText primary={<Typography fontFamily="roboto" fontWeight="700" p="5px" m="5px" backgroundColor="white" width="100px" borderRadius="20px" textAlign="center" boxShadow={2}>Precaution 3</Typography>} secondary={<Typography fontFamily="roboto" fontWeight="300" paddingLeft="15px"> ○ {Multiprecautions.precaution3}</Typography>} />
             </ListItem>
             <ListItem>
                 <ListItemText primary={<Typography fontFamily="roboto" fontWeight="700" p="5px" m="5px" backgroundColor="white" width="100px" borderRadius="20px" textAlign="center" boxShadow={2}>Precaution 4</Typography>} secondary={<Typography fontFamily="roboto" fontWeight="300" paddingLeft="15px"> ○ {Multiprecautions.precaution4}</Typography>} />
